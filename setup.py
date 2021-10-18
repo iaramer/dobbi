c6b5884a12abeb2f21fa2d8bfc6889d50f212b12
iaramer <yaramer95@gmail.com>
2021-10-18 22:05:28 +0300

    Added module

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='dobbi',
-    py_modules=['dobbi'],
+    py_modules=['dobbi', 'emoji', 'emoticons'],
     version='0.05',
     license='Apache License 2.0',
     description='An open-source NLP library: fast text cleaning and preprocessing.',
