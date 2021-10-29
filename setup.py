b503c22ee659032b90003bb37ca401b758de5fbc
iaramer <yaramer95@gmail.com>
2021-10-30 00:20:47 +0300

    Fixed encoding

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text(encoding='UTF-8')
 setup(
     name='dobbi',
     packages=['dobbi', 'dobbi.collections'],
-    version='0.12',
+    version='0.13',
     license='Apache License 2.0',
     description='An open-source NLP library: fast text cleaning and preprocessing.',
     long_description=long_description,
@@ -16,7 +16,7 @@ setup(
     author='Iaroslav Amerkhanov',
     author_email='amerkhanov.y@gmail.com',
     url='https://github.com/iaramer/dobbi',
-    download_url='https://github.com/iaramer/dobbi/archive/refs/tags/v0_12.tar.gz',
+    download_url='https://github.com/iaramer/dobbi/archive/refs/tags/v0_13.tar.gz',
     keywords=['nlp', 'text', 'string', 'regexp', 'preprocess', 'clean'],
     classifiers=[
         'Development Status :: 3 - Alpha',
