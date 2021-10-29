1638e2f356354d3f88b0753836eb323489c6bfc5
iaramer <yaramer95@gmail.com>
2021-10-30 00:19:40 +0300

    Fixed encoding

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding='UTF-8')
 
 setup(
     name='dobbi',
